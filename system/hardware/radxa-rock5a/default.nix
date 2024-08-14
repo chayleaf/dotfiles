ff493a9ed90c99e32eba1076968be574bd079186
chayleaf <chayleaf-git@pavluk.org>
2024-08-15 01:09:27 +0700

    Revert "server: temporarily switch to newer kernel"
    This reverts commit 449f873faaf3d15a03553d78c030534a1533e7d7.

--- system/hardware/radxa-rock5a/default.nix
+++ system/hardware/radxa-rock5a/default.nix
@@ -1,4 +1,4 @@
-{ pkgs-kernel2
+{ pkgs
 , config
 , ... }:
 
@@ -8,7 +8,7 @@
     "phy-rockchip-naneng-combphy"
   ];
 
-  boot.kernelPackages = pkgs-kernel2.linuxPackagesFor pkgs-kernel2.linux_latest;
+  boot.kernelPackages = pkgs.linuxPackagesFor pkgs.linux_latest;
 
   boot.kernelParams = [ "dtb=/${config.hardware.deviceTree.name}" ];
   hardware.deviceTree.enable = true;
